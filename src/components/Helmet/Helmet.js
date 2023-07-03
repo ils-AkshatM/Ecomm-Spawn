91b25d2c68195c4d32f88b96ecfbf9d70b700f5f
AkshatM17 <a.maheshwari@thesynapses.com>
2023-07-03 17:34:28 +0530

    replaced the favicon

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -3,7 +3,7 @@
 import React from 'react'
 
 const Helmet = ( props ) => {
-    document.title = "Spawn -" + props.title;
+    document.title = "Spawn - " + props.title;
   return <div className="w-100">{props.children}</div>;
 };
 
