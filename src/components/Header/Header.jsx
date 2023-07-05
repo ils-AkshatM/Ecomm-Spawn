bb5c6e0ba6ea90e370b6771b9801b89aeaa6fadb
AkshatM17 <a.maheshwari@thesynapses.com>
2023-07-05 12:41:56 +0530

    reflected the addition of products in the cart icon in the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,6 +9,7 @@ import logo from "../../assets/images/eco-logo.png";
 import userIcon from "../../assets/images/user-icon.png";
 
 import { Container, Row } from "reactstrap";
+import { useSelector } from "react-redux";
 
 const nav__links = [
   {
@@ -28,6 +29,7 @@ const nav__links = [
 const Header = () => {
 
   const headerRef = useRef(null);
+  const totalQuantity = useSelector(state=> state.cart.totalQuantity)
 
   const menuRef = useRef(null);
 
@@ -88,7 +90,7 @@ const Header = () => {
               </span>
               <span className="cart__icon">
                 <i class="ri-shopping-bag-line"></i>
-                <span className="badge">1</span>
+                <span className="badge">{totalQuantity}</span>
               </span>
 
               <span>
