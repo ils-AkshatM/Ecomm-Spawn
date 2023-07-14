7f378cc3bf0462f7e3fc2a625b99a6da87382a80
AkshatM17 <a.maheshwari@thesynapses.com>
2023-07-14 16:35:26 +0530

    updated the searching and sorting functionality

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,98 +1,78 @@
-import React,{ useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
 import CommonSection from '../components/UI/CommonSection';
 import Helmet from '../components/Helmet/Helmet';
 import { Container, Row, Col } from 'reactstrap';
-
-
 import '../styles/shop.css';
-
 import products from '../assets/data/products';
 import ProductsList from '../components/UI/ProductsList';
 
 const Shop = () => {
-
   const [productsData, setProductsData] = useState(products);
   const [sortOrder, setSortOrder] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
-  const handleFilter = e =>{
+  const handleFilter = (e) => {
     const filterValue = e.target.value;
-    if(filterValue === "sofa")
-    {
-      const filteredProducts = products.filter(
-        (item) => item.category === "sofa"
-      );
-      setProductsData(filteredProducts);
+    let filteredProducts = [];
+
+    if (filterValue === 'sofa') {
+      filteredProducts = products.filter((item) => item.category === 'sofa');
+    } else if (filterValue === 'mobile') {
+      filteredProducts = products.filter((item) => item.category === 'mobile');
+    } else if (filterValue === 'chair') {
+      filteredProducts = products.filter((item) => item.category === 'chair');
+    } else if (filterValue === 'watch') {
+      filteredProducts = products.filter((item) => item.category === 'watch');
+    } else if (filterValue === 'wireless') {
+      filteredProducts = products.filter((item) => item.category === 'wireless');
     }
 
-    if(filterValue === "mobile")
-    {
-      const filteredProducts = products.filter(
-        (item) => item.category === "mobile"
-      );
-      setProductsData(filteredProducts);
-    }
-
-    if(filterValue === "chair")
-    {
-      const filteredProducts = products.filter(
-        (item) => item.category === "chair"
-      );
-      setProductsData(filteredProducts);
-    }
-
-    if(filterValue === "watch")
-    {
-      const filteredProducts = products.filter(
-        (item) => item.category === "watch"
-      );
-      setProductsData(filteredProducts);
-    }
-
-    if(filterValue === "wireless")
-    {
-      const filteredProducts = products.filter(
-        (item) => item.category === "wireless"
-      );
-      setProductsData(filteredProducts);
-    }
-
+    setFilteredProducts(filteredProducts);
+    handleSearch('', filteredProducts);
   };
 
-  const handleSearch = e => {
-    const searchTerm = e.target.value
+  const handleSearch = (searchTerm, filteredProducts = []) => {
+    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+    let searchedProducts = [];
 
-    const searchedProducts = products.filter(item => item.productName.toLowerCase().includes(searchTerm.toLowerCase()))
+    if (filteredProducts.length > 0) {
+      searchedProducts = filteredProducts.filter((item) =>
+        item.productName.toLowerCase().includes(lowerCaseSearchTerm)
+      );
+    } else {
+      searchedProducts = products.filter((item) =>
+        item.productName.toLowerCase().includes(lowerCaseSearchTerm)
+      );
+    }
 
-    setProductsData(searchedProducts)
-  }
-  
+    setProductsData(searchedProducts);
+  };
 
   useEffect(() => {
     const handleSort = (order) => {
       const sortedProducts = [...productsData].sort((a, b) => {
-      if (order === 'ascending') {
-        return a.productName.localeCompare(b.productName);
-      } else if (order === 'descending') {
-        return b.productName.localeCompare(a.productName);
-      }
-      return 0;
-    });
+        if (order === 'ascending') {
+          return a.price - b.price;
+        } else if (order === 'descending') {
+          return b.price - a.price;
+        }
+        return 0;
+      });
 
-    setProductsData(sortedProducts);
-  };
+      setProductsData(sortedProducts);
+    };
+
     handleSort(sortOrder);
-  }, [productsData, sortOrder])
+  }, [productsData, sortOrder]);
 
-
-  return( 
-    <Helmet title='Shop'>
-      <CommonSection title='Products' />
+  return (
+    <Helmet title="Shop">
+      <CommonSection title="Products" />
 
       <section>
         <Container>
           <Row>
-            <Col lg='3' md='6'>
+            <Col lg="3" md="6">
               <div className="filter__widget">
                 <select onChange={handleFilter}>
                   <option>Filter By Category</option>
@@ -104,20 +84,24 @@ const Shop = () => {
                 </select>
               </div>
             </Col>
-            <Col lg='3' md='6' className='text-end'>
+            <Col lg="3" md="6" className="text-end">
               <div className="filter__widget">
-                <select onChange={(e)=> setSortOrder(e.target.value)}>
+                <select onChange={(e) => setSortOrder(e.target.value)}>
                   <option>Sort By</option>
                   <option value="ascending">Ascending</option>
                   <option value="descending">Descending</option>
                 </select>
               </div>
             </Col>
-            <Col lg='6' md='12'>
+            <Col lg="6" md="12">
               <div className="search__box">
-                <input type="text" placeholder='Search.......' onChange={handleSearch} />
+                <input
+                  type="text"
+                  placeholder="Search......."
+                  onChange={(e) => handleSearch(e.target.value, filteredProducts)}
+                />
                 <span>
-                  <i class="ri-search-line"></i>
+                  <i className="ri-search-line"></i>
                 </span>
               </div>
             </Col>
@@ -125,17 +109,19 @@ const Shop = () => {
         </Container>
       </section>
 
-      <section className='pt-0'>
+      <section className="pt-0">
         <Container>
           <Row>
-            {
-              productsData.length === 0? <h1 className='text-center'>No Products are found!</h1> : <ProductsList data={productsData} />
-            }
+            {productsData.length === 0 ? (
+              <h1 className="text-center">No Products are found!</h1>
+            ) : (
+              <ProductsList data={productsData} />
+            )}
           </Row>
         </Container>
       </section>
+    </Helmet>
+  );
+};
 
-  </Helmet>
-)}
-
-export default Shop
+export default Shop;
