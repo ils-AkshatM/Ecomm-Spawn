0d191c753380a94d6cc6c001c0d42238a8ace8cc
AkshatM17 <a.maheshwari@thesynapses.com>
2023-07-15 15:30:08 +0530

    contact us updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -25,50 +25,77 @@ const Contact = () => {
   return (
     <Helmet title="Contact Us">
       <CommonSection title="Contact Us" />
-      <section className="d-flex justify-content-center">
-        <div className="card contact-card">
-          <div className="card-body">
-            <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="name">Name:</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                />
+      <div className="container">
+        <div className="row">
+          <section className="contact-section col-lg-6">
+            <div className="contact-info">
+              <h4>Address:</h4>
+              <p className="mb-4">2880 Broadway, New York</p>
+              <h4>Contact Number:</h4>
+              <p className="mb-4">+1 123-456-7890</p>
+              <h4>Email:</h4>
+              <p className="mb-4">contact@example.com</p>
+            </div>
+          </section>
+          <section className="col-lg-6">
+            <div className="form-container">
+              <div className="card form-card">
+                <div className="card-body">
+                  <h2>Contact Form</h2>
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="name">Name:</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="email">Email:</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="message">Message:</label>
+                      <textarea
+                        className="form-control"
+                        id="message"
+                        rows="5"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        required
+                      ></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-primary buy__btn">
+                      Submit
+                    </button>
+                  </form>
+                </div>
               </div>
-              <div className="form-group">
-                <label htmlFor="email">Email:</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
+            </div>
+          </section>
+
+          <div className="col-lg-12">
+            <section className="map-section">
+              <div className="map-container">
+                {/* Add your map component or embed a map */}
+                {/* For example: */}
+                
               </div>
-              <div className="form-group">
-                <label htmlFor="message">Message:</label>
-                <textarea
-                  className="form-control"
-                  id="message"
-                  rows="5"
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  required
-                ></textarea>
-              </div>
-              <button type="submit" className="btn btn-primary buy__btn">
-                Submit
-              </button>
-            </form>
+            </section>
           </div>
-        </div>
-      </section>
+        </div>  
+      </div>
     </Helmet>
   );
 };
